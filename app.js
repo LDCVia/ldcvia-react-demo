002284d9523824e1b3b11330ccb3c69ad200bf4f
Matt White <matt@ldcvia.com>
2016-02-24 08:54:58 +0000

    Add edit and delete support

--- app.js
+++ app.js
@@ -1,24 +1,121 @@
 var Person = React.createClass({
+  editmode: false,
+  getInitialState: function() {
+    return {first_name: '', last_name: ''};
+  },
+  handleFirstNameChange: function(e) {
+    this.setState({first_name: e.target.value});
+  },
+  handleLastNameChange: function(e) {
+    this.setState({last_name: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var first_name = this.state.first_name.trim();
+    var last_name = this.state.last_name.trim();
+    if (!first_name || !last_name) {
+      return;
+    }
+    var person = this.state;
+    $.ajax({
+      url: this.props.host + "/document/" + this.props.database + "/Person/" + person.__unid,
+      dataType: 'json',
+      type: 'POST',
+      data: person,
+      xhrFields: {
+        withCredentials: true
+      },
+      complete: function(data) {
+        this.editmode = false;
+      }.bind(this)
+    });
+  },
   rawMarkup: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
     return { __html: rawMarkup };
   },
 
+  editDocument: function(){
+    this.setState({first_name: this.props.first_name, last_name: this.props.last_name, __unid: this.props.__unid})
+    this.editmode = true;
+  },
+
+  handleDelete: function(){
+    console.log("Deleting ");
+    console.log(this.props);
+    $.ajax({
+      url: this.props.host + "/document/" + this.props.database + "/Person/" + this.props.__unid,
+      dataType: 'json',
+      type: 'DELETE',
+      xhrFields: {
+        withCredentials: true
+      },
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   render: function() {
-    return (
-      <li className="list-group-item">
-        <div className="personName">
-          {this.props.first_name} {this.props.last_name}
-        </div>
-      </li>
-    );
+    if (this.editmode){
+      return (
+        <li className="list-group-item">
+          <form className="personForm" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="First name"
+              value={this.state.first_name}
+              onChange={this.handleFirstNameChange}
+              className="form-control"
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={this.state.last_name}
+              onChange={this.handleLastNameChange}
+              className="form-control"
+            />
+            <input type="submit" value="Post" className="btn btn-primary btn-block" />
+          </form>
+        </li>
+      )
+    }else{
+      return (
+        <li className="list-group-item">
+          <div className="personName">
+            {this.props.first_name} {this.props.last_name}
+            <div className="btn-group pull-right">
+              <input
+                type="button"
+                value="Edit"
+                onClick={this.editDocument}
+                id="editbutton"
+                className="btn btn-primary btn-xs"
+              />
+              <input
+                type="button"
+                value="Delete"
+                onClick={this.handleDelete}
+                id="delbutton"
+                className="btn btn-danger btn-xs"
+              />
+            </div>
+          </div>
+        </li>
+      );
+    }
   }
 });
 
 var PersonBox = React.createClass({
   loggedIn: false,
+  host: '',
 
   loadPeopleFromServer: function() {
+    this.host = this.props.host;
     if (this.loggedIn){
       $.ajax({
         url: this.props.host + "/collections/" + this.props.database + "/Person",
@@ -112,8 +209,10 @@ var PeopleList = React.createClass({
   render: function() {
     var peopleNodes = this.props.data.map(function(person) {
       return (
-        <Person first_name={person.first_name} last_name={person.last_name} key={person.__unid}>
-          {person.first_name} {person.last_name}
+        <Person first_name={person.first_name} last_name={person.last_name} __unid={person.__unid} key={person.__unid} host="https://eu.ldcvia.com/1.0" database="dev-londc-com-demos-200kpeople-nsf1444113120236">
+          {person.first_name}
+          {person.last_name}
+          {person.__unid}
         </Person>
       );
     });
@@ -214,7 +313,7 @@ var LoginForm = React.createClass({
             id="password"
             className="form-control"
           />
-          <input type="submit" value="Post" className="btn btn-primary btn-block" />
+          <input type="submit" value="Login" className="btn btn-primary btn-block" />
         </form>
       </div>
     );
